40c8da70cfd13eaefd6e68e7e2ad711648a35784
Diablo2004 <mehulmickey2004@gmail.com>
2024-01-30 23:33:22 +0530

    minor changes

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,8 +1,16 @@
-import React from "react";
 import "./style.scss";
 
+import ContentWrapper from "../../components/contentwrap/contentwrap";
+
 const Error = () => {
-  return <div>error</div>;
+  return (
+    <div className='Error'>
+      <ContentWrapper>
+        <span className='bigText'>404</span>
+        <span className='smallText'>Page not found!</span>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default Error;
