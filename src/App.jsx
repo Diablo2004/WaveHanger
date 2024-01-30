176c388b3607294d171e5ee9c5c73932f43238c0
Diablo2004 <mehulmickey2004@gmail.com>
2024-01-30 23:58:37 +0530

    minor errors fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import { getApiConfig, getGenres } from "./store/homeSlice";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
-import Details from "./pages/details/details";
-import SearchResult from "./pages/searchResult/searchResult";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
 import Error from "./pages/error/Error";
 
